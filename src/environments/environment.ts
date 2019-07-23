07fcdb7b7a84959fb9772870c1287167df1958ab
Sergiy Petruniv <petruniv.s.i@gmail.com>
2019-07-23 21:37:30 +0300

    Init (#4)
    * add login page and authentication
    
    * remade interceptors, add token to local storage
    
    * add teachers list
    
    * add jwt-decode, base routes & components
    
    * add constant.ts
    
    * add enviroments constant BASE URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  APIEndpoint: "http://35.228.86.127:8080/"
 };
 
 /*
