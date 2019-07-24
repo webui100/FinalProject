af5b1a7d7fb2ab81a5cc944715ab4308bfa5421d
Taras Shumeiko <shumeikotaras1@gmail.com>
2019-07-24 20:34:30 +0300

    added login to ngRx store

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,48 +1,41 @@
-import { AuthService } from "../services/auth.service";
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AuthService } from '../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
 
-import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
 import { login } from '../store/login/login.actions';
 
 @Component({
-  selector: "webui-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+  selector: 'webui-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   error: string;
-  // userData: Observable<object>;
 
   constructor(
     private auth: AuthService,
     private store: Store<{ login: object }>
-  ) {
-    // this.userData = store.pipe(select('login'));
-  }
+  ) {}
 
-    login: FormGroup = new FormGroup({
-    username: new FormControl("", Validators.required),
-    password: new FormControl("", Validators.required)
+  login: FormGroup = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
   });
 
   onSubmit(event): void {
     event.preventDefault();
 
     const data: { username: string, password: string } = {
-      password: this.login.get("username").value,
-      username: this.login.get("password").value
+      password: this.login.get('username').value,
+      username: this.login.get('password').value
     };
 
-    this.store.dispatch(login(data));
-
-    // console.log(this.userData);
-
     if (this.login.valid) {
-      this.auth.signIn(data)
+      this.store.dispatch(login(data));
+      this.auth.signIn();
     } else {
-      this.error = "Введіть логін та пароль"
+      this.error = 'Введіть логін та пароль';
     }
   }
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,57 +3,58 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import * as jwt_decode from '../../../node_modules/jwt-decode';
-import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+// import { Observable } from 'rxjs';
+// import { catchError, map, tap } from 'rxjs/operators';
 import { Store, select } from '@ngrx/store';
-import { selectLogin } from '../store/login/login.selector';
+// import { selectLogin } from '../store/login/login.selector';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  userData$: Observable<string>;
-  userData: string;
+  userData: any;
 
   constructor(
     private http: HttpClient,
     private router: Router,
     private store: Store<{ user }>
   ) {
-    this.userData$ = store.pipe(select('user'));
+    this.userData = store.pipe(select('user'));
   }
 
   private BASE_URI = environment.APIEndpoint;
 
+  signIn() {
+    const userData = {
+      username: this.userData.actionsObserver._value.username,
+      password: this.userData.actionsObserver._value.password,
+    };
 
-  signIn(userData) {
-    this.userData$.subscribe(data => this.userData = data);
-    console.log(this.userData);
     this.http
-      .post(this.BASE_URI + "signin", userData, {
+      .post(this.BASE_URI + 'signin', userData, {
         headers: new HttpHeaders({
-          "Content-Type": "application/json",
-          Accept: "*/*"
+          'Content-Type': 'application/json',
+          Accept: '*/*'
         }),
-        observe: "response"
+        observe: 'response'
       })
       .subscribe(response => {
-        const token = response.headers.get("Authorization");
+        const token = response.headers.get('Authorization');
         const decode_token = jwt_decode(token).Roles.authority;
-        localStorage.setItem("token", token);
+        localStorage.setItem('token', token);
 
-        if (decode_token === "ROLE_ADMIN") {
-          this.router.navigate(["/admin"]);
-        } else if (decode_token === "ROLE_USER") {
-          this.router.navigate(["/students"]);
-        } else if (decode_token === "ROLE_TEACHER") {
-          this.router.navigate(["/teachers"]);
+        if (decode_token === 'ROLE_ADMIN') {
+          this.router.navigate(['/admin']);
+        } else if (decode_token === 'ROLE_USER') {
+          this.router.navigate(['/students']);
+        } else if (decode_token === 'ROLE_TEACHER') {
+          this.router.navigate(['/teachers']);
         }
       });
   }
 
-  signOut() {
-    localStorage.removeItem("token");
-  }
+  // signOut() {
+  //   localStorage.removeItem('token');
+  // }
 }
