20311dcd1e5a7372c20be1a0a1739a44e9d6173a
Sergiy Petruniv <petruniv.s.i@gmail.com>
2019-07-27 09:19:51 +0300

    add store for teachers

--- src/app/services/teachers.service.ts
+++ src/app/services/teachers.service.ts
@@ -1,20 +1,34 @@
-import { Observable } from 'rxjs';
-import { environment } from './../../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-
-
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { environment } from "./../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Store, select } from "@ngrx/store";
+import { selectAll } from "../store/teachers/teachers.selector";
+import { teacherAction } from "../store/teachers/teachers.action";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TeachersService {
 
   private BASE_URI = environment.APIEndpoint;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private store: Store<{teachers}>) {
+  }
 
-  getTeachers(): Observable<any>{
-    return this.http.get(this.BASE_URI + "teachers");
+  getTeachers() { 
+    return this.http.get(`${this.BASE_URI}teachers`)
+    .subscribe(response => {
+      //@ts-ignore
+      this.store.dispatch(teacherAction({ data: response.data }));
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.getTeachers().unsubscribe();
+  }
+  ngOnInit(): void {
   }
 }
--- src/app/teachers/teachers.component.ts
+++ src/app/teachers/teachers.component.ts
@@ -1,9 +1,11 @@
+import { Store, select } from "@ngrx/store";
 import { Teacher } from "./../models/teacher";
 import { TeachersService } from "./../services/teachers.service";
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatTableDataSource } from "@angular/material/table";
 import { MatSort } from "@angular/material/sort";
+import { selectAll } from "../store/teachers/teachers.selector";
 
 @Component({
   selector: "webui-teachers",
@@ -11,7 +13,15 @@ import { MatSort } from "@angular/material/sort";
   styleUrls: ["./teachers.component.scss"]
 })
 export class TeachersComponent implements OnInit {
-  constructor(private teachers: TeachersService) {}
+  private data$: any;
+  data: any;
+
+  constructor(
+    private teachers: TeachersService,
+    private store: Store<{ teachers }>
+  ) {
+    this.data$ = this.store.pipe(select(selectAll));
+  }
 
   displayedColumns: string[] = [
     "firstname",
@@ -19,28 +29,33 @@ export class TeachersComponent implements OnInit {
     "patronymic",
     "dateOfBirth"
   ];
-  teachersList;
+  private teachersList: any;
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   getTeachers() {
-    this.teachers.getTeachers().subscribe(response => {
-      this.teachersList = new MatTableDataSource<Teacher>(response.data);
+    this.data$.subscribe(response => {
+      this.data = response;
+      this.teachersList = new MatTableDataSource<Teacher>(this.data);
       this.teachersList.paginator = this.paginator;
       this.teachersList.sort = this.sort;
-      
     });
+    if (!this.data) {
+      this.teachers.getTeachers();
+    }
   }
-  
-  applyFilter(filterValue: string) {
+
+  private applyFilter(filterValue: string) {
     this.teachersList.filter = filterValue.trim().toLowerCase();
   }
 
   ngOnInit() {
     this.getTeachers();
   }
-  ngAfterViewInit(){
-    this.teachersList.sort = this.sort;
+
+  ngOnDestroy(): void {
+    this.data$.unsubscribe()
+    
   }
 }
