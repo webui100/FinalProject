35000e70f852db03ad8ca58a5cccc95b1f2ae35f
Sergiy Petruniv <petruniv.s.i@gmail.com>
2019-07-28 12:07:49 +0000

    add expandable table,  (#12)
    * change teachers route
    
    * add store for teachers
    
    * fixed bug with routes and unsubscribe, add style
    
    * style login page
    
    * edit login page
    
    * table
    
    * teacher table styled

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -8,15 +8,19 @@ import {
 import { environment } from '../../environments/environment';
 import { loginReducer, State as LoginState } from './login/login.reducer';
 import { errorReducer, State as ErrorState } from './error/error.reducer';
+import { teachersDataReducer, State as TeachersState } from './teachers/teachers.reducer';
 
 export interface State {
   user: LoginState;
   errors: ErrorState;
+  teachers: TeachersState;
 }
 
 export const reducers: ActionReducerMap<State> = {
   user: loginReducer,
-  errors: errorReducer
+  errors: errorReducer,
+  teachers: teachersDataReducer,
+
 };
 
 export const metaReducers: MetaReducer<State>[] = !environment.production
---
+++ src/app/store/teachers/teachers.selector.ts
@@ -0,0 +1,13 @@
+import { createSelector } from '@ngrx/store';
+
+import { State as AppState } from '../index';
+import { State as TeachersState } from './teachers.reducer'
+
+export const selectTeachers = (state: AppState) => state.teachers;
+
+export const selectData = (state: TeachersState) => state.data;
+
+export const selectAll = createSelector(
+    selectTeachers,
+    selectData
+  );
