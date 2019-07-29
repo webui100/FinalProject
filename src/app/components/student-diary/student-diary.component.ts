1c23ecb18a111b4d2e6c7da8385f49b5c5c08c20
Taras Shumeiko <shumeikotaras1@gmail.com>
2019-07-29 10:58:25 +0300

    added buttons for navigate weeks

--- src/app/components/student-diary/student-diary.component.ts
+++ src/app/components/student-diary/student-diary.component.ts
@@ -20,6 +20,7 @@ export class StudentDiaryComponent implements OnInit {
     'П\'ятниця'
   ];
   dayNumbers: number[];
+  currentDate: any = new Date();
 
   constructor(
     private studentDiary: StudentDiaryService,
@@ -29,25 +30,58 @@ export class StudentDiaryComponent implements OnInit {
   }
 
   ngOnInit() {
-    const date = new Date(2019, 4, 17);
+    const date = this.currentDate;
     const weekDaysPassed = date.getDay() > 0 ? date.getDay() - 1 : date.getDay() + 6;
     date.setDate(date.getDate() - weekDaysPassed);
 
     const year = date.getFullYear();
-    const month = date.getMonth() < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
+    const month = date.getMonth() < 9 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
     const day = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
 
     const dayNumbers = [];
-    this.weekDays.map((item, i) => dayNumbers.push(+day + i));
+    const daysInMonth = this.daysInMonth(year, month);
+    this.weekDays.map((item, i) => {
+      if (+day + i <= daysInMonth) {
+        dayNumbers.push(+day + i);
+      } else {
+        dayNumbers.push(+day + i - daysInMonth);
+      }
+    });
     this.dayNumbers = dayNumbers;
 
     this.studentDiary.fetchStudentDiary(`${year}-${month}-${day}`);
     this.diary$.subscribe(data => this.diary = data.diary);
   }
 
+  daysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+  }
+
+  selectNextWeek() {
+    const year = this.currentDate.getFullYear();
+    const month = this.currentDate.getMonth();
+    const day = this.currentDate.getDate() + 7;
+    this.currentDate = new Date(year, month, day);
+    this.ngOnInit();
+  }
+
+  selectPreviousWeek() {
+    const year = this.currentDate.getFullYear();
+    const month = this.currentDate.getMonth();
+    const day = this.currentDate.getDate() - 7;
+    this.currentDate = new Date(year, month, day);
+    this.ngOnInit();
+  }
+
+  selectCurrentWeek() {
+    this.currentDate = new Date();
+    this.ngOnInit();
+  }
+
   showDiary() {
     console.log(this.diary);
     console.log(this.weekDays);
     console.log(this.dayNumbers);
+    console.log(this.currentDate);
   }
 }
