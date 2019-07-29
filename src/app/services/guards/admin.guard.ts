07e64d035ae7705f0ef9b8d53af3dfa8a5f47f3b
Novsky <djnovsky@gmail.com>
2019-07-29 10:13:34 +0300

    ngrx routing, guards, logout

--- src/app/services/guards/admin.guard.ts
+++ src/app/services/guards/admin.guard.ts
@@ -5,9 +5,10 @@ import {AuthService} from '../auth.service';
 @Injectable()
 export class AdminGuard implements CanActivate{
 
-  constructor(private _authService: AuthService, private _route: Router){}
-  canActivate(): boolean {
-    if (this._authService.loggedIn() && (this._authService.role === 'ROLE_ADMIN')) {
+  constructor(private _authService: AuthService, private _route: Router) {}
+  
+  canActivate(): boolean{
+    if(this._authService.loggedIn() && (this._authService.role === 'ROLE_ADMIN')){
       return true;
     } else {
       this._route.navigate(['']);
