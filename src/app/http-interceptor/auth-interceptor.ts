e636917a1091b502754505149ea033e08170384d
TarasShumeiko <39749305+TarasShumeiko@users.noreply.github.com>
2019-07-30 08:26:36 +0300

    Student diary (#14)
    * some changes
    
    * initial commit from student-diary
    
    * added diary for each days
    
    * fixed student diary
    
    * added buttons for navigate weeks
    
    * added DayPicker
    
    * implemented student-diary component
    
    * implemented student-diary component

--- src/app/http-interceptor/auth-interceptor.ts
+++ src/app/http-interceptor/auth-interceptor.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-
 import { Observable } from 'rxjs';
+
 import { AuthService } from '../services/auth.service';
+import { StudentDiaryService } from '../services/student-diary.service';
 
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private auth: AuthService) {}
+  constructor(
+    private auth: AuthService,
+    private studentDiary: StudentDiaryService
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('token');
