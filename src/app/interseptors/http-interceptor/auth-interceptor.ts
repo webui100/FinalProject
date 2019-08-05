290e278e717c362253bde5986a1fe8bde33324a2
Novsky <djnovsky@gmail.com>
2019-08-05 22:50:41 +0300

    current user, guards, refresh token

--- src/app/interseptors/http-interceptor/auth-interceptor.ts
+++ src/app/interseptors/http-interceptor/auth-interceptor.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-
 import {AuthService} from '../../services/auth.service';
-import {Observable} from 'rxjs/internal/Observable';
+import { Observable } from 'rxjs';
 
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(public auth: AuthService) {
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('token');
